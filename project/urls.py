d2a7b3d87fcdbe3f622def9793b25d885a48f656
metzker87 <julianometzker87@gmail.com>
2023-01-17 22:57:41 -0300

    modified urls.py file with view example

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse("This is a HttpResponse from my server to user request")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
