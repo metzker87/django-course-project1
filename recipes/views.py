c0f1b333449a81072a010542e6bed50fc6395ec4
metzker87 <julianometzker87@gmail.com>
2023-01-20 11:29:10 -0300

    Removed contactus and aboutus pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -7,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Juliano Metzker',
     })
-
-
-def aboutus(request):
-    return HttpResponse("Contact us - Page")
-
-
-def contactus(request):
-    return render(request, 'recipes/contactus.html')
